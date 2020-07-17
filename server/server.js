0f5f78c6517b16ee41d49208de2b20d30b24b343
kram-iv <vikram.karanth@gmail.com>
2020-07-18 01:42:32 +1000

    Create CRUD API's

--- server/server.js
+++ server/server.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const User = require('./models/User');
+const { urlencoded } = require('body-parser');
+
+dotenv.config()
 
 const app = express()
+
+mongoose.connect(
+process.env.DATABASE,
+{ useNewUrlParser: true, useUnifiedTopology: true },
+  err=> {
+    if(err) {
+        console.log(err)
+    }
+    else {
+        console.log('Connected to the mongodb server!!')
+    }
+})
+
+// Middlewares
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -13,8 +34,30 @@ app.get('/',(req,res) => {
 })
 
 app.post('/',(req,res) => {
-    console.log(req.body.name)
+    // console.log(req.body.name)
+    let user = new User()
+    user.name = req.body.name
+    user.email = req.body.email
+    user.password = req.body.password
+
+    user.save(err => {
+      if (err) {
+        res.json(err)
+      } else {
+        res.json("Successfully saved!!")
+      }
+    })
 })
+
+// require api's
+const productRoutes = require('./routes/product')
+const categoryRoutes = require('./routes/category')
+const ownerRoutes = require('./routes/owner')
+
+app.use('/api', productRoutes)
+app.use('/api', categoryRoutes)
+app.use('/api', ownerRoutes)
+
 
 app.listen(3000, (err) => {
     if(err) {
